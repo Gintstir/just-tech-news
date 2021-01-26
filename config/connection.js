ca881e7e0efbb28c4384cb600099429f4357a28c
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-26 10:16:58 -0500

    basic file structure set up

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+//create connection to our database, pass in your mySQL information for UN and PW
+//the 'new Sequelize()' function accepts the databse name, mysql un and pw respectively as params.  
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
